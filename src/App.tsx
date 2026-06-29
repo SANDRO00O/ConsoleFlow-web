141eebc5bfa65d86bc7f43ef4cd089358fc592c0
SANDRO00O <mk63cp@gmail.com>
2026-06-29 10:11:16 +0300

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -24,13 +24,6 @@ function Layout({ children }: { children: React.ReactNode }) {
       <nav className="fixed top-4 w-full z-50">
         <div className="max-w-6xl mx-auto px-4 sm:px-6 flex items-center justify-between">
           <Link to="/" className="flex items-center h-12 bg-black/40 backdrop-blur-xl border border-white/10 rounded-full pl-2 pr-5 shadow-lg shadow-black/20 hover:bg-white/5 transition-colors">
-            <div className="w-9 h-9 flex items-center justify-center">
-              <img 
-                src="/icon-512.png" 
-                alt="ConsoleFlow Logo" 
-                className="w-full h-full object-contain scale-[1.6]"
-              />
-            </div>
             <img 
               src="https://raw.githubusercontent.com/SANDRO00O/ConsoleFlow-mobile/master/screenshots/banner.svg" 
               alt="ConsoleFlow" 
